64ff211fc173a58e6d7b31b9f5f7ce15e7fb6d70
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-12-04 23:50:29 +0300

    Parcer implemented

--- TestsGeneratorLibrary/SourceCodeParcer.cs
+++ TestsGeneratorLibrary/SourceCodeParcer.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace TestsGeneratorLibrary
 {
@@ -10,17 +13,38 @@ namespace TestsGeneratorLibrary
     {
         public ParcingInfo Parce(string sourceCode)
         {
-            return new ParcingInfo(new List<ClassInfo>());
+            SyntaxTree tree = CSharpSyntaxTree.ParseText(sourceCode);
+            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+            return new ParcingInfo(GetClasses(root));
         }
 
-        private List<ClassInfo> GetClasses()
+        private List<ClassInfo> GetClasses(CompilationUnitSyntax root)
         {
-            return new List<ClassInfo>();
+            string className, classNamespace;
+            List<ClassInfo> classes = new List<ClassInfo>();
+
+            foreach (ClassDeclarationSyntax classDeclaration in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
+            {
+                classNamespace = ((NamespaceDeclarationSyntax)classDeclaration.Parent).Name.ToString();
+                className = classDeclaration.Identifier.ValueText;
+                classes.Add(new ClassInfo(className, classNamespace, GetMethods(classDeclaration)));
+            }
+            return classes;
         }
 
-        private List<MethodInfo> GetMethods()
+        private List<MethodInfo> GetMethods(ClassDeclarationSyntax classDeclaration)
         {
-            return new List<MethodInfo>();
+            string methodName;
+            List<MethodInfo> classMethods = new List<MethodInfo>();
+
+            // public only
+            foreach (MethodDeclarationSyntax methodDeclaration in classDeclaration.DescendantNodes().OfType<MethodDeclarationSyntax>()
+                .Where((methodDeclaration) => methodDeclaration.Modifiers.Any((modifier) => modifier.IsKind(SyntaxKind.PublicKeyword))))
+            {
+                methodName = methodDeclaration.Identifier.ValueText;
+                classMethods.Add(new MethodInfo(methodName));
+            }
+            return classMethods;
         }
     }
 }
