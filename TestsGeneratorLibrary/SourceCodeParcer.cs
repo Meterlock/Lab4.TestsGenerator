93c3e3eb06184a904c2b446fe5f2c8145b127abd
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-12-04 22:06:16 +0300

    Parcer structure determined

---
+++ TestsGeneratorLibrary/SourceCodeParcer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+    class SourceCodeParcer
+    {
+        public ParcingInfo Parce(string sourceCode)
+        {
+            return new ParcingInfo(new List<ClassInfo>());
+        }
+
+        private List<ClassInfo> GetClasses()
+        {
+            return new List<ClassInfo>();
+        }
+
+        private List<MethodInfo> GetMethods()
+        {
+            return new List<MethodInfo>();
+        }
+    }
+}
