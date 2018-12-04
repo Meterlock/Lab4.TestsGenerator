e4d611875b660dc8185e9a37068db4c7822cc1d2
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-12-04 21:46:56 +0300

    Added class which contains all classes information

---
+++ TestsGeneratorLibrary/ParcingInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+    class ParcingInfo
+    {
+        public ParcingInfo(List<ClassInfo> classes)
+        {
+            Classes = classes;
+        }
+
+        public List<ClassInfo> Classes { get; }
+    }
+}
