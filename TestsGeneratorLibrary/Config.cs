0ca3adb9bd509b98c0b5de0abeb6fd3070acd72b
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-12-05 13:17:44 +0300

    Created config for generator

---
+++ TestsGeneratorLibrary/Config.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+    class Config
+    {
+        public Config(int _MaxReadFiles, int _MaxProcessingTasks, int _MaxWriteFiles)
+        {
+            MaxReadFiles = _MaxReadFiles;
+            MaxProcessingTasks = _MaxProcessingTasks;
+            MaxWriteFiles = _MaxWriteFiles;
+        }
+
+        public int MaxReadFiles { get; set; }
+        public int MaxProcessingTasks { get; set; }
+        public int MaxWriteFiles { get; set; }        
+    }
+}
