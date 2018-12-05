816eb189ca53015fd1d4b69ee7f6a1138aeaa6b6
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-12-05 13:05:34 +0300

    Async reader implemented

---
+++ TestsGeneratorLibrary/AsyncReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TestsGeneratorLibrary
+{
+    class AsyncReader
+    {
+        public async Task<string> Read(string path)
+        {
+            using (StreamReader strmReader = new StreamReader(path))
+            {
+                return await strmReader.ReadToEndAsync();
+            }
+        }
+    }
+}
