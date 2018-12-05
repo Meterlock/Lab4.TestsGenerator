44ca6fc700680a11560ce9ebf5b1a7cae64b0f97
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-12-05 13:32:55 +0300

    TestsGenerator class' structure determined

---
+++ TestsGeneratorLibrary/TestsGenerator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsGeneratorLibrary
+{
+    class TestsGenerator
+    {
+        public TestsGenerator(Config _config)
+        {
+            config = _config;
+        }
+
+        private Config config;
+
+        public void Generate(List<string> inputFiles, string outputPath)
+        {
+        }
+    }
+}
