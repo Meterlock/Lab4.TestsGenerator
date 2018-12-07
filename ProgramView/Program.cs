f39decab4d4d3900836e9df2642e2b97822010db
Meterlock <36419855+Meterlock@users.noreply.github.com>
2018-12-07 13:17:20 +0300

    Main method updated, bug fix

--- ProgramView/Program.cs
+++ ProgramView/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TestsGeneratorLibrary;
 
 namespace ProgramView
 {
@@ -10,6 +11,21 @@ namespace ProgramView
     {
         static void Main(string[] args)
         {
+            int maxReadFiles = 2;
+            int maxProcessingTasks = 3;
+            int maxWriteFiles = 4;
+            var cfg = new Config(maxReadFiles, maxProcessingTasks, maxWriteFiles);
+
+            var sourceFiles = new List<string>();
+            sourceFiles.Add(@"D:\УНИВЕР\5 семестр\СПП\Test\input\Tracer.cs");
+            sourceFiles.Add(@"D:\УНИВЕР\5 семестр\СПП\Test\input\Writer.cs");
+            string outputPath = @"D:\УНИВЕР\5 семестр\СПП\Test\output";
+
+            var generator = new TestsGenerator(cfg);
+            generator.Generate(sourceFiles, outputPath).Wait();
+
+            Console.WriteLine("Generation ended");
+            Console.ReadKey();
         }
     }
 }
